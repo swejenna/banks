0338a588db6832681693573e24d4bdf700b04714
Jay <108143448+swejenna@users.noreply.github.com>
2025-09-09 13:09:50 -0800

    changed banks to local Alaskan banks/credit unions

--- src/app/banks.ts
+++ src/app/banks.ts
@@ -1,13 +1,13 @@
 import { Bank } from './bank';
 
 export const BANKS: Bank[] = [
-    { id: 11, name: 'Bank of America Merrill Lynch', website: 'https://www.bankofamerica.com', address: 'New York, NY' },
-    { id: 12, name: 'Barclays Capital', website: 'https://www.ib.barclays', address: 'London, UK' }, 
-    { id: 13, name: 'Citi', website: 'https://www.citi.com', address: 'New York, NY' }, 
-    { id: 14, name: 'Credit Suisse', website: 'https://www.credit-suisse.com', address: 'Zurich, Switzerland' }, 
-    { id: 15, name: 'Deutsche Bank', website: 'https://www.db.com', address: 'Frankfurt, Germany' }, 
-    { id: 16, name: 'Goldman Sachs', website: 'https://www.goldmansachs.com', address: 'New York, NY' }, 
-    { id: 17, name: 'J.P. Morgan', website: 'https://www.jpmorgan.com', address: 'New York, NY' }, 
-    { id: 18, name: 'Morgan Stanley', website: 'https://www.morganstanley.com', address: 'New York, NY' }, 
-    { id: 19, name: 'UBS', website: 'https://www.ubs.com', address: 'Zurich, Switzerland' }
+    { id: 1, name: 'First National Bank Alaska', website: 'https://www.fnbalaska.com', address: 'Anchorage, AK' },
+    { id: 2, name: 'Northrim Bank', website: 'https://www.northrim.com', address: 'Anchorage, AK' }, 
+    { id: 3, name: 'Denali State Bank', website: 'https://www.denalistatebank.com', address: 'Fairbanks, AK' }, 
+    { id: 4, name: 'Mt. McKinley Bank', website: 'https://www.mtmckinleybank.com', address: 'Fairbanks, AK' }, 
+    { id: 5, name: 'First Bank of Alaska', website: 'https://www.firstbankak.com', address: 'Ketchikan, AK' }, 
+    { id: 6, name: 'Global Credit Union', website: 'https://www.globalcu.com', address: 'Anchorage, AK' }, 
+    { id: 7, name: 'Credit Union 1', website: 'https://www.cu1.org', address: 'Anchorage, AK' }, 
+    { id: 8, name: 'Matanuska Valley Federal Credit Union', website: 'https://www.mvfcu.coop/index.html', address: 'Palmer, AK' }, 
+    { id: 9, name: 'Northern Skies Federal Credit Union', website: 'https://www.nsfcu.org', address: 'Anchorage, AK' }
 ];
